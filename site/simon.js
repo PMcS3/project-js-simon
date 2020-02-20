7cee92ebb72f62672b00e72239b254d67ce8c41f
Patrick Sullivan <pasullivan@davidson.edu>
2020-02-20 12:13:28 -0800

    Simon rough pseudo

--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,33 @@
-// This file contains the game logic.
-// All the event-listening should happen in buttons.js
+let buttons = console.require("./buttons");
+let begin = false;
+
+function start() {
+begin = true;
+let highlightColors = ["green","red","yellow","blue"];
+let currentColor = "";
+}
+
+function greenHighlight() {
+  $('.simon-button.green').*transform*
+}
+
+function select() {
+
+}
+
+function userClick() {
+  $('.simon-button').on('click', function() {
+
+  });
+}
+
+function moves(begin) {
+  if (begin === true) {
+    greenHighlight();
+    currentColor = highlightColors[0];
+    if (currentColor === "green")
+    {
+
+    }
+  }
+}
